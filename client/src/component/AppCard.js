98dc34683b120ebb3f9d63965117b27e0a746f12
Brody S <brody.sanford@gmail.com>
2022-08-24 16:14:57 -0400

    going to start working on backend

---
+++ client/src/component/AppCard.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import { Card, Button, Icon } from "semantic-ui-react";
+
+function AppCard({id, link, name, category, ratings, description, icons, screenshots}){
+    
+    return(
+        <Card>
+                <Card.Content class="header">
+                    <h1>{name}</h1>
+                    <img src={icons.medium} alt="app's icon"/>
+                </Card.Content >
+
+                <Card.Content class="header">
+                    <a href={link}>Store Link</a>
+                </Card.Content>
+
+                <Card.Content  class="header">
+                    <h4>Category:</h4> 
+                    {category}
+                </Card.Content >
+
+                <Card.Content  class="header">
+                    <h4>Rating:</h4>
+                    {ratings.average}
+                </Card.Content >
+
+                <Card.Content  class="header">
+                <h4>Description:</h4>
+                    {description}
+                </Card.Content>
+
+                <Card.Content class="header">
+                    <h4>Screenshot:</h4>
+                    <img src={screenshots[0]} alt="app's screenshot #1"/>
+                </Card.Content >
+
+                <Button >Favorite</Button>
+                {/* <Button ><Link to={`/${id}/RequestDetail`}>   
+                     Update Requeast
+                     </Link>
+                </Button> */}
+                {/* <Button animated onClick={handleClick}>
+                    <Button.Content visible >Delete Request?</Button.Content>
+                    <Button.Content hidden>
+                        <Icon name='close' />
+                    </Button.Content>
+                </Button> */}
+        </Card>
+    )
+}
+
+export default AppCard;
---
+++ client/src/component/AppsList.js
@@ -0,0 +1,59 @@
+import React from "react";
+import AppCard from "./AppCard";
+import { Container, Card } from 'semantic-ui-react';
+// import { Card, Form, Label, Button, Input } from 'semantic-ui-react';
+import { useState, useEffect } from 'react';
+
+function AppsList({store}){
+
+    const [apps, setApps] = useState([])
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '273a166339mshd767c60ebd4c95cp1aea2cjsn3f5b1c4f3066',
+            'X-RapidAPI-Host': 'app-stores.p.rapidapi.com'
+        }
+    };
+    
+    const terms = ['cool', 'dumb', 'pretty', 'fast', 'popular', 'trendy', 'sophisticated', 'advanced', 'hacks', 'steamy', 'old', 'vague']
+    let term = terms[Math.floor(Math.random()*terms.length)];
+
+
+    useEffect(() => {
+        fetch(`https://app-stores.p.rapidapi.com/search?store=${store}&term=${term}&language=en`, options)
+        .then(response => response.json())
+        .then(response => setApps(response))
+        .catch(err => console.error(err));
+    }, [])
+
+     console.log(apps)
+
+     const appsList = apps.map(app => {
+        return <AppCard key={app.id}
+        id={app.id}
+        link={app.url}
+        name={app.name}
+        category={app.category}
+        ratings={app.ratings}
+        description={app.description}
+        icons={app.icons}
+        screenshots={app.screenshots}
+        // handleDelete={handleDelete}
+        />
+    })
+
+     const nouns = ['creations', 'recommendations', 'iterations']
+     let noun = nouns[Math.floor(Math.random()*nouns.length)];
+
+    return(
+        <Container textAlign="left"> 
+        <h1>How about these {noun}?</h1>
+        <Card.Group itemsPerRow={1}>
+            {appsList}
+        </Card.Group>
+    </Container>
+    )
+}
+
+export default AppsList;
