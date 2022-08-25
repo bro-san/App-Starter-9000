d96dfa9d08586f98df4ce783ff7762e0cb31e5fe
Brody S <brody.sanford@gmail.com>
2022-08-25 00:07:05 -0400

    about to reseed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :user
-    belongs_to :program
+    # belongs_to :program
     has_many :responses
 
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,8 +1,8 @@
 class Program < ApplicationRecord
     has_many :favorites
-    has_many :comments  
+    # has_many :comments  
     has_many :users, through: :favorites
-    has_many :commenters, through: :comments, source: "User"
+    # has_many :commenters, through: :comments, source: "User"
     validates :name, uniqueness: true
 
 end
