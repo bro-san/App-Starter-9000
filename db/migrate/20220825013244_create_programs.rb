27200e74b79deb381529f43f421ddd0fd2d98955
Brody S <brody.sanford@gmail.com>
2022-08-24 22:25:34 -0400

    about to migrate

--- db/migrate/20220825013244_create_programs.rb
+++ db/migrate/20220825013244_create_programs.rb
@@ -1,7 +1,14 @@
 class CreatePrograms < ActiveRecord::Migration[6.1]
   def change
     create_table :programs do |t|
-      
+      t.string :name
+      t.string :link
+      t.string :category
+      t.string :rating
+      t.string :description
+      t.string :icon
+      t.string :screenshot
+
       t.timestamps
     end
   end
