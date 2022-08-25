73ffcce5dc9c80f554deaa489d7ac5712e46f0df
Brody S <brody.sanford@gmail.com>
2022-08-24 21:57:52 -0400

    added models back in

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :program
+    has_many :responses
+
+end
---
+++ app/models/program.rb
@@ -0,0 +1,8 @@
+class Program < ApplicationRecord
+    has_many :favorites
+    has_many :comments  
+    has_many :users, through: :favorites
+    has_many :commenters, through: :comments, source: "User"
+    validates :name, uniqueness: true
+
+end
